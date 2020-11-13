93a43f1f7ff1a829eb8bbd4e04b4559ceefb61a9
Chris Johnson <Johnny.C.Alexander@gmail.com>
2020-11-13 13:56:54 -0600

    links established, homepage router established

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import {HashRouter as Router, Route} from 'react-router-dom';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import HomePage from '../Home/HomePage';
 import './App.css';
 
 class App extends Component {
@@ -9,9 +10,24 @@ class App extends Component {
       <div className="App">
         <h1>Movies!</h1>
         <Router>
-          {/* ADD PAGES! */}
+          <nav>
+            <button>
+              <Link to="/">Home</Link> {/* shows full list of movies*/}
+            </button>
+            <button>
+              <Link to="/details">Movie Details</Link>{' '}
+              {/* has button to return to home */}
+            </button>
+            <button>
+              <Link to="add">Add Movie</Link>{' '}
+              {/*save and cancel button back to home*/}
+            </button>
+          </nav>
+          <br />
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/details" component={''} />
+          <Route exact path="/add" component={''} />
         </Router>
-        <p>Empty Page</p>
       </div>
     );
   }
